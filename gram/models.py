4234db7f045ee296306cf72b287f1331188b8cf0
Nyakinyua <wanyakinyua968@gmail.com>
2019-12-21 10:53:00 +0300

    create profile class and add columns to the class

--- gram/models.py
+++ gram/models.py
@@ -2,9 +2,24 @@ from django.db import models
 import datetime as dt 
 
 # Create your models here.
+class Profile(models.Model):
+    user_name = models.CharField(max_length=40)
+    bio = models.CharField(max_length=200)
+    profile_photo = models.ImageField(upload_to = 'profile/')
+    
+    def __str__(self):
+        return self.user_name
+    
+    def save_user(self):
+        return self.save()
+
 class Images(models.Model):
     image = models.ImageField(upload_to = 'images/')
     image_name = models.CharField(max_length=30)
-    caption = models.CharField(max-length=100)
+    caption = models.CharField(max_length=100)
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
-    likes =models.IntegerField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.image_name
+   
