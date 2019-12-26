ab43fffde8f145b80f1ed947f9239203b04746a8
Nyakinyua <wanyakinyua968@gmail.com>
2019-12-26 16:03:48 +0300

    add new columns in the images table and add methods such as save and delete to both the profile and images tables

--- gram/models.py
+++ gram/models.py
@@ -4,17 +4,20 @@ import datetime as dt
 # Create your models here.
 class Profile(models.Model):
     user_name = models.CharField(max_length=40)
-    bio = models.CharField(max_length=200)
-    profile_photo = models.ImageField(upload_to = 'profile/')
+    bio = models.TextField(max_length=200)
+    profile_photo = models.ImageField(upload_to = 'profile/',blank=True)
     
     def __str__(self):
         return self.user_name
     
-    def save_user(self):
+    def save_profile(self):
         return self.save()
+    
+    def delete_profile(self):
+        return self.delete()
 
 class Images(models.Model):
-    image = models.ImageField(upload_to = 'images/')
+    image = models.ImageField(upload_to = 'images/',blank=True)
     image_name = models.CharField(max_length=30)
     caption = models.CharField(max_length=100)
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
@@ -22,4 +25,14 @@ class Images(models.Model):
     
     def __str__(self):
         return self.image_name
+    
+    class Meta:
+        ordering = ['pub_date']
+        
+    def save_image(self):
+        return self.save()
+    
+    def delete_image(self):
+        return self.delete()
+       
    
