99b45ef482d0e4ef17d18db17ef5ca992df73267
Nyakinyua <wanyakinyua968@gmail.com>
2019-12-26 16:50:24 +0300

    add comment table and likes table in the models.py

--- gram/models.py
+++ gram/models.py
@@ -15,12 +15,29 @@ class Profile(models.Model):
     
     def delete_profile(self):
         return self.delete()
+    
+class Like(models.Model):
+    like = models.IntegerField(max_length=50,blank=True,default='0')
+    
+class Comment(models.Model):
+    comment = models.TextField(max_length=45,blank=True)
+
+    def save_comment(self):
+        return self.save()
+
+    def delete_comment(self):
+        return self.delete()
+
+    def __str__(self):
+        return self.comment
 
 class Images(models.Model):
     image = models.ImageField(upload_to = 'images/',blank=True)
     image_name = models.CharField(max_length=30)
     caption = models.CharField(max_length=100)
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment,on_delete=models.CASCADE)
+    like = models.ForeignKey(Like,on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
@@ -35,4 +52,8 @@ class Images(models.Model):
     def delete_image(self):
         return self.delete()
        
-   
+    @classmethod
+    def get_all_images(cls):
+        images = cls.objects.all()
+        return images
+    
