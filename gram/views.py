8287357cf066c21f2280393f80f02f3a00232ca5
Nyakinyua <wanyakinyua968@gmail.com>
2019-12-27 09:03:01 +0300

    import http404 to send an error incase profile id does not exist

--- gram/views.py
+++ gram/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse  
-
+from django.http import HttpResponse ,Http404 
+from .models import Images,Profile
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world, Welcome to Instagram")
+    title = "Nyakinyua Gram Photos"
+    images = Images.get_all_images()
+    return render(request,"index.html",{"title":title,"images":images})
+
+def profile(request,id):
+    try:
+        profile = Profile.objects.get(id=id)
+        posts = 
+    except DoesNotExist:
+        raise Http404()
+    return render(request,'profile.html',{'profile':profile})
